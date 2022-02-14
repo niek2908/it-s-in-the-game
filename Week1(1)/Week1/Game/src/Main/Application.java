7b30be91e5fd28bce6c5f5a69a1959e13c0204f6
niek <niek2908@gmail.com>
2022-02-14 22:34:02 +0100

    started working on a drawtemplate method

--- Week1(1)/Week1/Game/src/Main/Application.java
+++ Week1(1)/Week1/Game/src/Main/Application.java
@@ -3,16 +3,21 @@ package Main;
 import nl.saxion.app.CsvReader;
 import nl.saxion.app.SaxionApp;
 
+import java.awt.*;
+
 public class Application implements Runnable {
     public static void main(String[] args) {
-        SaxionApp.start(new Application(), 1024, 768);
+        SaxionApp.start(new Application(), 1900, 1000);
     }
 
     public void run() {
-        CsvReader save = new CsvReader("save.csv");
+        CsvReader save = new CsvReader("Week1(1)/Week1/Game/src/Main/save.csv");
         SaxionApp.printLine("to get to the next screen press a key");
         SaxionApp.readChar();
+        SaxionApp.clear();
         int difficulty;
+        drawTemplate("option1","option2","option3","option4");
+        SaxionApp.readChar();
         SaxionApp.printLine("please select a difficulty setting");
         SaxionApp.printLine("1: easy");
         SaxionApp.printLine("2: medium");
@@ -23,5 +28,12 @@ public class Application implements Runnable {
 
     }
 
+public void drawTemplate(String option1,String option2,String option3,String option4){
+        SaxionApp.turnFillOff();
+        SaxionApp.setBorderColor(Color.black);
+        SaxionApp.drawRectangle(50,50,1750,650);
 
+        
+
+}
 }
